d24b98b8e5856dceb4574e1ae10c4144f9e06c74
juanluna96 <juandluna17@gmail.com>
2021-02-26 13:25:16 -0500

    Creando el Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Formulario = () => {
     return (
-        <h1>Formulario aqui</h1>
+        <form method="post" className="col-12">
+            <fieldset className="text-center">
+                <legend>Busca bebidas por categoría o ingrediente</legend>
+            </fieldset>
+
+            <div className="row mt-3">
+                <div className="col-md-4">
+                    <input className="form-control" type="text" name="nombre" placeholder="Buscar por ingrediente" />
+                </div>
+                <div className="col-md-4">
+                    <select id="categoria" className="custom-select" name="categoria">
+                        <option>-- Seleccione una categoría --</option>
+                    </select>
+                </div>
+                <div className="col-md-4">
+                    <button className="btn btn-primary btn-block" type="submit">Buscar bebidas</button>
+                </div>
+            </div>
+        </form>
     )
 }
 
